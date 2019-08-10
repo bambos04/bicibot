531ab90eeb195e613716dd55ba3b44562776eff0
bambos04 <48250476+bambos04@users.noreply.github.com>
2019-08-10 11:07:40 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -21,13 +21,81 @@ if(strpos($text, "/start") === 0 || stristr ( $text, "/command1" ) == true )
 {
 	$response = "Ciao Amici delle bici";
 }
-elseif( stristr ( $text, "/bici" ) == true )
+elseif( stristr ( $text, "/nibali" ) == true )
 {
-	$response = "clette";
+	$response = "ilnostrovincenzonibali";
 }
-elseif( stristr ( $text, "/aa" ) == true )
+elseif( stristr ( $text, "/attacca" ) == true )
 {
-	$response = "bb";
+	$response = "Kruijswijk attacca cristo santo";
+}
+elseif( stristr ( $text, "/genovesi" ) == true )
+{
+	$response = "Ape regina";
+}
+elseif( stristr ( $text, "/beppeconti" ) == true )
+{
+	$response = "ma quante ne hai timbrate con sta retorica su Coppi e Bartali";
+}
+elseif( stristr ( $text, "/ciccone" ) == true )
+{
+	$response = "ilnostrogiuliociccone, futurodelciclismoitaliano";
+}
+elseif( stristr ( $text, "/alanphilippe" ) == true )
+{
+	$response = "forse intendevi: Alan Philippe";
+}
+elseif( stristr ( $text, "/sgarbozza" ) == true )
+{
+	$response = "104";
+}
+elseif( stristr ( $text, "/nick" ) == true )
+{
+	$response = "Nuyens";
+}
+elseif( stristr ( $text, "/tedescone" ) == true )
+{
+	$response = "Burghardt, Kittel, Martin, Degenkolb, insomma un tedesco qualsiasi";
+}
+elseif( stristr ( $text, "/francesino" ) == true )
+{
+	$response = "Bardet, Barguil, Gaudu insomma un francese qualsiasi";
+}
+elseif( stristr ( $text, "/eccolo" ) == true )
+{
+	$response = "https://t.me/cambioshimano2box/13841";
+}
+elseif( stristr ( $text, "/scatto" ) == true )
+{
+	$response = "https://t.me/cambioshimano2box/13842";
+}
+elseif( stristr ( $text, "/ribaltone" ) == true )
+{
+	$response = "https://t.me/cambioshimano2box/13843";
+}
+elseif( stristr ( $text, "/aru" ) == true )
+{
+	$response = "https://t.me/cambioshimano2box/13844";
+}
+elseif( stristr ( $text, "/avversariboh" ) == true )
+{
+	$response = "https://t.me/cambioshimano2box/13845";
+}
+elseif( stristr ( $text, "/neve" ) == true )
+{
+	$response = "https://t.me/cambioshimano2box/13846";
+}
+elseif( stristr ( $text, "/aspettare" ) == true )
+{
+	$response = "https://t.me/cambioshimano2box/13847";
+}
+elseif( stristr ( $text, "/trionfa" ) == true )
+{
+	$response = "https://t.me/cambioshimano2box/13848";
+}
+elseif( stristr ( $text, "/dumoulin" ) == true )
+{
+	$response = "https://t.me/cambioshimano2box/13849";
 }
 // altri comandi si aggiungono con altri elseif uguali a quello sopra.
 $parameters = array('chat_id' => $chatId, "text" => $response);
