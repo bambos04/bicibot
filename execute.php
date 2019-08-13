c8f4a514498f263551c882c945f4ae138f549fcd
bambos04 <48250476+bambos04@users.noreply.github.com>
2019-08-13 10:23:18 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -8,15 +8,15 @@ if(!$update)
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
+$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";//non lo prende perché fa riferimento alla chat da cui viene invocato e non all'utente che lo invoca.
 $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
 $text = trim($text);
-$text = strtolower($text);
 header("Content-Type: application/json");
-if(strpos($text, "/start") === 0 || stristr ( $text, "/command1" ) == true )
+$response = '';
+if(strpos($text, "/start") === 0 || stristr ( $text, "/ciao" ) == true )
 {
 	$response = "Ciao Amici delle bici";
 }
