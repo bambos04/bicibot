ffc6c292b9d6c0692761ffe5dd8357b5dae8f033
bambos04 <48250476+bambos04@users.noreply.github.com>
2019-08-13 10:16:10 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -5,7 +5,6 @@ if(!$update)
 {
   exit;
 }
-// non riesco ancora a capire come far inserire nella risposta del bot dati che si riferiscono all'invocante dello stesso.
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
@@ -15,8 +14,8 @@ $username = isset($message['chat']['username']) ? $message['chat']['username'] :
 $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
 $text = trim($text);
+$text = strtolower($text);
 header("Content-Type: application/json");
-$response = '';
 if(strpos($text, "/start") === 0 || stristr ( $text, "/command1" ) == true )
 {
 	$response = "Ciao Amici delle bici";
